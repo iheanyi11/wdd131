bde11144e3f4cf4b1a9525be195f17691b935182
iheanyi11 <ejeagbaakuegbo@gmail.com>
2024-06-12 00:43:46 +0100

    The html, css and js of the base homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Output the current year in the footer's first paragraph
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Output the date the document was last modified in the footer's second paragraph
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
