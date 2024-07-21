3723fafa707833dba129366dae8d4986aca9e97e
iheanyi11 <ejeagbaakuegbo@gmail.com>
2024-07-21 20:53:03 +0100

    home page

---
+++ projects/scripts/home.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navbar = document.getElementById('navbar');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navbar.classList.toggle('active');
+    });
+}); 
